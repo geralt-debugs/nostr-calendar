2658b49643dcb7ccf1bb78ebda45dd9f619a23b0
Rama <16651792+geralt-debugs@users.noreply.github.com>
2026-01-16 14:46:55 +0100

    Mobile app (#28)
    * added capacitor for android and ios
    
    * some updates
    
    * added app scriüts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.formstr.calendar',
+  appName: 'Calendar',
+  webDir: 'dist'
+};
+
+export default config;
