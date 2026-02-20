68df3e18308c897e741fda8d2c3edff47df63fc1
rama <16651792+geralt-debugs@users.noreply.github.com>
2026-02-20 17:17:18 +0100

    added some documentation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "app.formstr.calendar",
-  appName: "Formstr Calendar",
+  appName: "Calendar by Form*",
   webDir: "dist",
 };
 
